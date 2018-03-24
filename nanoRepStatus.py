6c5c5a675c50ecd4ede2484b570764a966777668
qcryptic <kylerump@outlook.com>
2018-03-24 09:51:19 -0500

    Allow different context roots

--- nanoRepStatus.py
+++ nanoRepStatus.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 #curl -g -d '{ "action": "version" }' 
 
+context_root = "/"
 title = 'Stunts\' Nano Node'
 rpc_address = '[::1]:7076'
 account = 'xrb_1stuntsgoycyqjdzszzzkqj3nzyj44kgtxt7prf6sapo37tb7znp3h7usw7w'
@@ -13,9 +14,9 @@ account = 'xrb_1stuntsgoycyqjdzszzzkqj3nzyj44kgtxt7prf6sapo37tb7znp3h7usw7w'
 #headers = {'content-type': 'application/json'}
 #r = requests.post(url, data=payload, headers=headers)
 
-@app.route("/")
-def restSystemInfo():
-    return render_template('template.html', title=title, account=account)
+@app.route(context_root)
+def page():
+    return render_template('template.html', root=context_root, title=title, account=account)
 
 if __name__ == '__main__':
     app.run(debug=True,port=7575)
