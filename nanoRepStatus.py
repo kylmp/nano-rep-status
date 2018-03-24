42c9e0bf473fed6884cbc0f33882a9747559146c
qcryptic <kylerump@outlook.com>
2018-03-24 09:57:43 -0500

    Add application root

--- nanoRepStatus.py
+++ nanoRepStatus.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 
 #curl -g -d '{ "action": "version" }' 
 
-context_root = "/"
 title = 'Stunts\' Nano Node'
 rpc_address = '[::1]:7076'
 account = 'xrb_1stuntsgoycyqjdzszzzkqj3nzyj44kgtxt7prf6sapo37tb7znp3h7usw7w'
+app.config["APPLICATION_ROOT"] = "/status"
 #payload = open("request.json")
 #headers = {'content-type': 'application/json'}
 #r = requests.post(url, data=payload, headers=headers)
 
-@app.route(context_root)
-def page():
-    return render_template('template.html', root=context_root, title=title, account=account)
+@app.route("/")
+def restSystemInfo():
+    return render_template('template.html', title=title, account=account)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=7575)
+    app.run(port=7575)
