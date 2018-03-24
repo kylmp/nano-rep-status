f864d61f252ac8c81949a1ef8d47f549c5ed029f
qcryptic <kylerump@outlook.com>
2018-03-24 13:46:32 -0500

    Add rpc info

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# nano-rep-status
-Status page for nano representatives
+# Nano Representative Status Page
+
+### About
+Status page for nano representatives. Shows online status, balances, voting weight and more.
+
+Created by stunts - /u/quiteCryptic 
+
+### Installation 
+
+**Prerequisite:** Your nano node should already be running and accessible via RPC from the system this status page is running on.
+
+**Install Dependencies (Debian/Ubuntu):**
+```
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3
+sudo apt-get install python3-pip
+```
+
+**Clone Repo:**
+```
+cd ~
+git clone https://github.com/qcryptic/nano-rep-status.git
+python ./nano-rep-status/
+```
+
+######Donate: xrb_38z7oxody6ubdm8awbxjgyjc3j7axcwnfmqdhcz7tj48pyzj79qodq1aq95n
--- nanoRepStatus.py
+++ nanoRepStatus.py
@@ -1,22 +1,34 @@
 from flask import Flask
 from flask import render_template
-#import requests
+import nano
 
 app = Flask(__name__)
 
-#curl -g -d '{ "action": "version" }' 
-
 title = 'Stunts\' Nano Node'
-rpc_address = '[::1]:7076'
-account = 'xrb_1stuntsgoycyqjdzszzzkqj3nzyj44kgtxt7prf6sapo37tb7znp3h7usw7w'
-app.config["APPLICATION_ROOT"] = "/status"
-#payload = open("request.json")
-#headers = {'content-type': 'application/json'}
-#r = requests.post(url, data=payload, headers=headers)
+rpc = nano.rpc.Client('http://ip6-localhost:7076')
+acc = 'xrb_1stuntsgoycyqjdzszzzkqj3nzyj44kgtxt7prf6sapo37tb7znp3h7usw7w'
 
 @app.route("/")
-def restSystemInfo():
-    return render_template('template.html', title=title, account=account)
+def load_page():
+    return render_template('template.html', title=title, account=acc)
+
+@app.route("/info")
+def get_info():
+    version = (rpc.version()["node_vendor"])
+    acc_info = rpc.account_info(acc, True, True, True)
+    peers = len(rpc.peers())
+    blocks = rpc.block_count()
+    response = {
+        "account":acc,
+        "representative":acc_info["representative"],
+        "balance":acc_info["balance"],
+        "weight":acc_info["weight"],
+        "peers":peers,
+        "blocks":blocks["count"],
+        "unchecked":blocks["unchecked"],
+        "version":version
+    }
+    return response
 
 if __name__ == '__main__':
     app.run(port=7575)
